f37d1370a473d93c8cbc115309ff0478017e9aac
Gabriel Passos <gabriel80546@gmail.com>
2023-11-13 09:44:00 -0300

    ./push.sh

--- main.c
+++ main.c
@@ -2,7 +2,16 @@
 #include<stdlib.h>
 #include<string.h>
 
+typedef struct linkedList {
+    void *dados;
+    struct linkedList *next;
+} linkedList_t;
+
 int main() {
+
+
+
     printf("Hello World!\n");
+
     return 0;
 }
